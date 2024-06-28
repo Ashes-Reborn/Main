549aab3c5493b7712dc4d7e9268477892a33205b
Stephen-PP <pierrepaulstephen@gmail.com>
2024-06-28 01:20:38 -0400

    initial commit

---
+++ src/main/java/com/ashesreborn/main/MainMod.java
@@ -0,0 +1,10 @@
+package com.ashesreborn.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MainMod.MOD_ID)
+public class MainMod {
+    public final static String MOD_ID = "ar_main";
+
+
+}
